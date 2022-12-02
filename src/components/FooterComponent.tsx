7f391e959a3b22aae8035c00e12489b45508ac82
kompi12 <nika.hoi3@gmail.com>
2022-12-02 14:51:45 +0100

    Fixed name

--- src/components/FooterComponent.tsx
+++ src/components/FooterComponent.tsx
@@ -4,7 +4,7 @@ export default function FooterComponent() {
       <div className='flex justify-center bg-gray-700 py-5 text-left text-white '>
         <div className=''>
           <div>RADNO VRIJEME 09-00</div>
-          <div> TELEFON 01 4813 394</div> © {new Date().getFullYear()} Noktunro
+          <div> TELEFON 01 4813 394</div> © {new Date().getFullYear()} Nokturno
         </div>
       </div>{' '}
     </>
